4eca202307668817aafafdf5794457e5c061919a
prashant <prashant.aghara2111@gmail.com>
2023-07-08 17:21:05 +0530

    Numbers & Float

---
+++ num_info.py
@@ -0,0 +1,33 @@
+num = 3
+print(type(num))
+num1 = 3.14
+print(type(num1))
+
+# Arithmatic Operators
+print(2 + 2)
+print(2 - 1)
+print(2 * 2)
+print(2 / 2)
+print(3 // 2)  # Floor division
+print(3 ** 2)  # Exponent
+print(3 % 2)
+
+num2 = 1
+print(num2)
+num2 += 1
+print(num2)
+print(abs(-2))
+print(round(3.75, 1))
+
+# Comparisons Ops
+num3 = 1
+num4 = 1
+print(num3 == num4)
+print(num3 != num2)
+print(num3 >= num4)
+print(num2 < num3)
+
+# Casting
+num_1 = int('100')
+num_2 = int('200')
+print(num_1 + num_2)
