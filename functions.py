fc91b812c3eefd335de152a86f1aed500868f7ae
prashant <prashant.aghara2111@gmail.com>
2023-07-08 18:49:30 +0530

    Functions

---
+++ functions.py
@@ -0,0 +1,46 @@
+def hello_func():
+    print('HELLO')
+
+
+print(hello_func)
+hello_func()
+
+
+def hello(greeting, name='YOU'):
+    return '{} , {}'.format(greeting, name)
+
+
+print(hello('HI'))
+print(hello('HI', 'Prashant'))
+
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+student_info('Math', 'JAVA', name='Prashant', age='23')
+
+# Number of days per month. First value placeholder for indexing purposes.
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def is_leap(year):
+    """Return True for leap years, False for non-leap years."""
+
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def days_in_month(year, month):
+    """Return number of days in that month in that year."""
+
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+
+print(days_in_month(2016, 2))
