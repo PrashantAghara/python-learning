49ed0102940ec0f0ca42efa9a7ca87fe3de3ecd9
prashant <prashant.aghara2111@gmail.com>
2023-07-10 22:14:40 +0530

    Modules

---
+++ import_module.py
@@ -0,0 +1,10 @@
+# import my_module as mm
+from my_module import find_index, test
+import sys
+import antigravity
+
+courses = ['Math', 'Java', 'Python']
+index = find_index(courses, 'Java');
+print(index)
+
+print(sys.path)
